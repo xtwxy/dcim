98de03bb3a2797679b925f46129a98ebc538b9f3
System Master <master@localhost.localdomain>
2017-04-20 14:52:29 +0800

    implementing mp3000.

--- mp3000/src/main/java/com/wincom/fsu/mp3000/internal/MP3000CodecImpl.java
+++ mp3000/src/main/java/com/wincom/fsu/mp3000/internal/MP3000CodecImpl.java
@@ -1,31 +1,56 @@
 package com.wincom.fsu.mp3000.internal;
 
+import com.wincom.dcim.agentd.AgentdService;
 import com.wincom.dcim.agentd.Codec;
 import java.util.HashMap;
 import java.util.Map;
 
 /**
  * Composition of TCP connections to a MP3000.
+ *
  * @author master
  */
 public class MP3000CodecImpl extends Codec.Adapter {
-    Map<String, MP3000CodecChannelImpl> outbound = new HashMap<>();
-    
+
+    private String HOST;
+    private final int BASE_PORT;
+    private final int PORT_COUNT;
+    private final Map<Integer, MP3000CodecChannelImpl> outbound;
+
+    public MP3000CodecImpl(
+            String host,
+            int basePort,
+            int portCount,
+            AgentdService agent
+    ) {
+        this.outbound = new HashMap<>();
+        this.HOST = host;
+        this.BASE_PORT = basePort;
+        this.PORT_COUNT = portCount;
+
+        for (int i = 0; i < this.PORT_COUNT; ++i) {
+            outbound.put(i,
+                    new MP3000CodecChannelImpl(
+                            this.HOST,
+                            this.BASE_PORT + i,
+                            agent
+                    )
+            );
+        }
+    }
+
     /**
-     * Connect <code>Codec</code> to <code>Codecchannel</code> 
-     * with identifier <code>channelId</code>.
-     * 
+     * Connect <code>Codec</code> to <code>Codecchannel</code> with identifier
+     * <code>channelId</code>.
+     *
      * @param channelId the identifier of the <code>CodecChannel</code>.
      * @param cc the <code>Codec</codec> to be connected.
      */
     @Override
     public void setOutboundCodec(String channelId, Codec cc) {
-        outbound.get(channelId).setCodec(cc);
+        int port = Integer.parseInt(channelId);
+        if (port >= 0 && port < this.PORT_COUNT) {
+            outbound.get(port).setCodec(cc);
+        }
     }
-
-    @Override
-    public void onWriteComplete() {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-    
 }
